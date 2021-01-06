fb8b618c344678a62c6d809dd883cc8b724e3b5a
Deepak Gupta <69721318+guptadeepak46474@users.noreply.github.com>
2021-01-06 20:43:03 +0530

    Add files via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j = 0; j < n-i-1; j++)
+        {
+            if(arr[j] > arr[j+1])
+                swap(&arr[j],&arr[j+1]);
+        }
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int arr[] = {11,3,66,8,59};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr,n);
+    cout<<"BubbleSorted Array is: ";
+    printArray(arr,n);
+    return 0;
+}
